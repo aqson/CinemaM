bf6348ca15d3540360207c02860788578887f473
Artem Yavorsky <aqson@me.com>
2013-05-07 13:31:39 +0300

    uploading project
    CinemaM is application that help people order places in your cinema, at
    the conference or for other performance?

---
+++ README.md
@@ -0,0 +1,23 @@
+#CinemaM 
+
+Need application that help people order places in your cinema, at the conference or for other performance? CinemaM is what you need! It works with open-source platform [Meteor](http://meteor.com/main) and it is ultra-simple!
+###Installation:  
+**Install Meteor:**  
+```$ curl https://install.meteor.com | /bin/sh```
+  
+**Create your project:**  
+```$ meteor create your_project```   
+
+**Insert this app files to *your_project*** 
+
+**Run it locally:**   
+  
+``` 
+$ cd CinemaM 
+$ meteor 
+ => Meteor server running on: http://localhost:3000/ 
+ ```
+**For more information visit [Meteor Docs](http://docs.meteor.com/).**
+
+####[DEMO](http://cinemam.meteor.com/)
+ 
---
+++ placesMakerMeteor.js
@@ -0,0 +1,52 @@
+Places = new Meteor.Collection('places');
+Messages = new Meteor.Collection('messages')
+
+if (Meteor.isClient) {
+  Meteor.subscribe("places");
+  var OrderedPlaces = []
+
+  Template.allPlaces.places = function () {
+    return Places.find({},{sort: {title: 1}});
+  };
+  Template.placeItem.placeEvent = function () {
+    return Session.get(this.title)
+  }
+
+  Template.placeItem.events({
+      'click div': function () {
+        if (this.ordered === 0 && !~OrderedPlaces.indexOf(this._id)) {
+          OrderedPlaces.push(this._id);
+          Session.set(this.title,'selected')
+        }
+        else if (this.ordered === 0 && ~OrderedPlaces.indexOf(this._id)){
+          OrderedPlaces.splice(OrderedPlaces.indexOf(this._id),1)
+          Session.set(this.title,'simple')
+
+      }
+    }
+  })
+  Template.order.events({
+    'click div' : function () {
+      OrderedPlaces.forEach(function(id){
+        var currentPlace = Places.find({_id:id}).fetch()[0].title
+        Places.update(id,{$set:{ordered:1,placeClass:'ordered'}});
+        Session.set(currentPlace,"");
+        OrderedPlaces = [];
+      })
+    }
+  })
+};
+
+if (Meteor.isServer) {
+  Meteor.publish("places",function(){
+    return Places.find({})
+  });
+  Meteor.startup(function(){
+    if (Places.find().count() === 0 ) {
+      for (var i = 0; i < 100; i++) {
+        Places.insert({title:i+1,ordered:0,placeClass:"",id:i+1})
+      }
+    }
+  });
+}
+
